b9300e6d72ed6d3f92c9873e0235506f3621812e
OSTARASYSTEMS\Nimisha.Sharma <nimisha.sharma@ostarasystems.com>
2025-04-09 16:48:04 +0100

    git project

---
+++ Test Suites/TS01.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>TS01</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>8402546d-0bdd-4519-b9f9-bcf3c02958ea</testSuiteGuid>
+   <testCaseLink>
+      <guid>df59989b-a2ff-4e39-a74e-c6255fbf807c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
